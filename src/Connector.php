06d94763c873ef36f9510f7f9c6dd16695e58bb9
gg97g <giuseppettigiorgia97@gmail.com>
2019-11-12 16:57:53 +0100

    Aggiunta cartella src e classe connector

---
+++ src/Connector.php
@@ -0,0 +1,14 @@
+<?php namespace WomPlatform\Connector;
+
+class Connector{
+public function isconnected($bool = true)
+  {
+    return $bool;
+  }
+  public function hasconnected($var = 100)
+  {
+    return $var;
+  }
+
+}
+?>
