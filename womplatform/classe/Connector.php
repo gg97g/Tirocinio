285050d970c158baf7274cac86ffe8c3d3613835
gg97g <giuseppettigiorgia97@gmail.com>
2019-12-21 16:24:46 +0100

    Aggiunta cartella womplatform

---
+++ womplatform/classe/Connector.php
@@ -0,0 +1,11 @@
+<?php namespace womPlatform\Connector;
+
+class Connector
+{
+  public function __construct()
+  {
+    die("Hello");
+  }
+}
+
+?>
---
+++ womplatform/start.php
@@ -0,0 +1,5 @@
+<?php
+
+//require_once 'classe/Connector.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+?>
